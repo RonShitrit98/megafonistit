51007de014ff10cb252b04024cca97b073b617b1
Ron Shitrit <ronshitrit456@gmail.com>
2023-04-24 12:07:57 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <h1>HOME</h1>
+    <song-list />
+  </main>
+</template>
+
+<script setup>
+import songList from "./components/song-list.vue";
+</script>
---
+++ src/components/song-list.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li v-for="song in songs" :key="song.title" @click="openSong(song)">
+        {{
+          song.title
+        }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { songService } from "../services/song-service";
+export default {
+  data() {
+    return {
+      songs: songService.getSongs(),
+    };
+  },
+  methods:{
+    openSong(song){
+        console.log(song.body)
+    }
+  }
+};
+</script>
+]
