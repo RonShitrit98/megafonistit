b9e5cf8d5b44bd26526e41e9867fc0e93520d3fc
Ron Shitrit <ronshitrit456@gmail.com>
2023-04-24 15:34:18 +0300

    all songs added

--- src/components/song-display.vue
+++ src/components/song-display.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>{{ song.title }}</h1>
+    <h6 v-if="song.notes">{{ song.notes }}</h6>
     <p v-for="line in displaySong" :key="line">{{ line }}</p>
     <h3>סימן:</h3>
     <img :src="song.sign" :alt="song.sign" />
--- src/services/song-service.js
+++ src/services/song-service.js
@@ -17,8 +17,8 @@ function getSongs() {
       recording: "SOON",
     },
     {
-      title: "לא נעזוב (שם זמני)",
-      notes:'יש לשיר כל בית פעמיים',
+      title: "הו הו הו הו",
+      notes: "יש לשיר כל בית פעמיים",
       body: "לא נעזוב*נצא אל הרחוב*לא תהיה פה עריצות הרוב!*-*רציתם הפיכה*קיבלתם מחאה*לא נעצור עד שתהיה חוקה*-*אם לא יהיה שוויון*נפיל את השלטון*נפלתם על הדור לא נכון*-* הגיע הזמן*שינוי מתחיל מכאן* סוף כל סוף העם שלי מוכן*",
       sign: "https://thumbs.dreamstime.com/b/human-cartoon-hand-showing-four-fingers-number-87864701.jpg",
       recording: "SOON",
@@ -33,6 +33,36 @@ function getSongs() {
       title: "לא מפחדות",
       body: "אנחנו לא מפחדות*אנחנו לא מפחדות*אנחנו לא מפחדות*אנחנו לא מפחדות!",
       sign: "https://media.istockphoto.com/id/1247824082/vector/l-loser-hand-sign.jpg?s=612x612&w=0&k=20&c=qUZ70JBrgfEZDKMV9wEujIKOrGYgiSWtx_viwXSlUuw=",
+      recording: "SOON",
+    },
+    {
+      title: "נרדוף אתכם בכל מקום",
+      body: "נרדוף אתכם בכל מקום,*נבוא לכם גם בחלום*לא נתן לכם דקה של מנוחה-*הגיעה המהפכה!*לא הונגרה לא פולין,*לך מפה יריב לוין*לא ניתן לכם להציל את השקרן-*ישראל זאת לא איראן!",
+      sign: "https://media.istockphoto.com/id/476603184/photo/hand-walking-on-fingers.jpg?s=612x612&w=0&k=20&c=20k2mvWwNuC-p3yxHUV0b047vrUShdhH4QgId4fchP8=",
+      recording: "SOON",
+    },
+    {
+      title: "אמרו שבחורף ניפול",
+      body: "אמרו שבחורף ניפול*אבל הם טעו בגדול*כי לא נפסיק לצעוד*בשביל עתיד ורוד*ביבי פילג את העם*ביב שיקר לכולם*נבוא בכל שבת*נפיל את המושחת*הבית בעזרה רועד*הבית בעזה רועד*וביבי מפחד*וביבי מפחד",
+      sign: "https://thumbs.dreamstime.com/b/thumb-down-male-hand-sign-isolated-white-background-close-up-140718107.jpg",
+      recording: "SOON",
+    },
+    {
+      title: "דור שלם דורש עתיד ורוד",
+      body: "דור שלם דורש עתיד ורוד!",
+      sign: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/LG_WHISEN_%EC%86%90%EC%97%B0%EC%9E%AC_%EC%A7%80%EB%A9%B4_%EA%B4%91%EA%B3%A0_%EC%B4%AC%EC%98%81_%EC%82%AC%EC%A7%84_%2830%29_hand_only.jpg/220px-LG_WHISEN_%EC%86%90%EC%97%B0%EC%9E%AC_%EC%A7%80%EB%A9%B4_%EA%B4%91%EA%B3%A0_%EC%B4%AC%EC%98%81_%EC%82%AC%EC%A7%84_%2830%29_hand_only.jpg",
+      recording: "SOON",
+    },
+    {
+      title: "אנחנו נתקן",
+      body: "כן!*וכן!*וכן וכן וכן!*הרסתם את המדינה ואנחנו נתקן!",
+      sign: "https://media.istockphoto.com/id/165982630/photo/woman-hand-showing-peace-sign-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=XtH1HpEiViXfwtqFOJXuX7olo1LKO74rI_0fdzwYnTY=",
+      recording: "SOON",
+    },
+    {
+      title: "כל העולם כולו",
+      body: "כל העולם כולו*גשר צר מאד*גשר צר מאד*גשר צר מאד*כל העולם כולו*גשר צר מאד*גשר צר מאד*והעיקר, והעיקר*לא לפחד, לא לפחד כלל*והעיקר והעיקר*לא לפחד כלל",
+      sign: "https://www.shutterstock.com/image-photo/human-hand-palm-down-260nw-172294937.jpg",
       recording: "SOON",
     },
   ];
